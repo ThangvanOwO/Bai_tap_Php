d62bd04fdad24a2e040f8d5c964e87942cc0ebfe
ThangvanOwO <vanvthang@students.hueic.edu.vn>
2025-09-11 16:21:23 +0700

    tinhtoan va so sanh

---
+++ tinhtoan.php
@@ -0,0 +1,43 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+$tong = $a + $b;
+echo "tong cua $a va $b la: $tong<br>";
+
+$hieu = $a - $b;
+echo "hieu cua $a va $b la: $hieu<br>";
+
+$tich = $a * $b;
+echo "tich cua $a va $b la: $tich<br>";
+
+$thuong = $a / $b;
+echo "thuong cua $a va $b la: $thuong<br>";
+
+$du = $a % $b;
+echo "du cua $a va $b la: $du<br>";
+
+$luy_thua = $a ** $b;
+echo "luy thua cua $a va $b la: $luy_thua<br>";
+
+$can_bac_hai = sqrt($a);
+echo "can bac hai cua $a la: $can_bac_hai<br>";
+
+
+$c = 34;
+$d = 45;
+
+$so_sanh = $c == $d;
+echo "so sanh cua $c va $d la: $so_sanh<br>";
+
+$so_sanh = $c != $d;
+echo "so sanh cua $c va $d la: $so_sanh<br>";
+
+$so_sanh = $c > $d;
+echo "so sanh cua $c va $d la: $so_sanh<br>";
+
+$so_sanh = $c < $d;
+echo "so sanh cua $c va $d la: $so_sanh<br>";
+
+?>
