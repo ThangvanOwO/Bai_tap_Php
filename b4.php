8ab171c520b6b7d76d6b5ba68e07743662fe4f75
ThangvanOwO <vanvthang@students.hueic.edu.vn>
2025-09-11 15:49:22 +0700

    ham var_dump

---
+++ b4.php
@@ -0,0 +1,9 @@
+<?php
+// var_dump la ham de in ra gia tri cua bien float , int , string , boolean , array , object , null;
+// vi du:
+// $a = 10;
+// var_dump($a);
+// ket qua: int (10);
+$a = 10.4837438434;
+var_dump($a);
+?>
