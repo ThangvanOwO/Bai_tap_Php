b9db239812cc6e508ad8d7afb2895955b8c166a2
ThangvanOwO <vanvthang@students.hueic.edu.vn>
2025-09-16 10:21:49 +0700

    mang lien ket

---
+++ manglienket.php
@@ -0,0 +1,13 @@
+<?php
+$giaraucu = array(
+    "cà rốt" => 20000,
+    "củ cải" => 15000,
+    "su hào" => 25000,
+    "bắp cải" => 30000,
+    "cải thảo" => 35000
+);
+echo " Giá cà rốt: " . $giaraucu["cà rốt"] . " VND <br>";
+echo " Giá bắp cải: " . $giaraucu["bắp cải"] . " VND <br>";
+echo " Giá cải thảo: " . $giaraucu["cải thảo"] . " VND <br>";
+echo " Giá củ cải: " . $giaraucu["củ cải"] . "  VND <br>";
+?>
