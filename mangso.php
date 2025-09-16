7027facd3c87d6b58a6ccb3394f37ce65bd20933
ThangvanOwO <vanvthang@students.hueic.edu.vn>
2025-09-16 10:21:08 +0700

    mang so

---
+++ mangso.php
@@ -0,0 +1,19 @@
+<?php
+$so = array(1, 2, 3, 4, 5 , 6, 7, 8, 9, 10);
+foreach ($so as $value) {
+    echo "Số: $value <br>";
+}   
+$so [0] = "một"; 
+$so [1] = "hai";
+$so [2] = "ba";
+$so [3] = "bốn";
+$so [4] = "năm";
+$so [5] = "sáu";
+$so [6] = "bảy";
+$so [7] = "tám";
+$so [8] = "chín";
+$so [9] = "mười";
+foreach ($so as $value) {
+       echo "Số: $value <br>";
+}
+?>
