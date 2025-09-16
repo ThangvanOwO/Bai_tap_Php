8e0d3c62aa34722eb14192de6c8177dc9f4d29fa
ThangvanOwO <vanvthang@students.hueic.edu.vn>
2025-09-16 10:24:36 +0700

    ham function XD

---
+++ ham_function.php
@@ -0,0 +1,13 @@
+<?php
+function tinhTong($a, $b) {
+    return $a + $b;
+}
+
+
+$ketqua = tinhTong(5, 3); 
+echo "tổng là :$ketqua <br>";
+function Xinchao($ten = "bạn"){
+    echo "Xin chào " . $ten . "<br>";
+}
+Xinchao("Thắng");
+?>
